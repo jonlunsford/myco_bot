3a98c4e7917a84b3310bed55b29bea187082993f
Jon Lunsford <jon@capturethecastle.net>
2020-10-14 14:35:07 -0700

    listen device change events

--- lib/myco_bot/instrumenter.ex
+++ lib/myco_bot/instrumenter.ex
@@ -3,20 +3,21 @@ defmodule MycoBot.Instrumenter do
 
   def setup do
     events = [
-      #[:myco_bot, :started],
+      # [:myco_bot, :started],
 
       [:myco_bot, :ht_sensor, :error],
-      [:myco_bot, :ht_sensor, :read_temp],
+      # [:myco_bot, :ht_sensor, :read_temp],
       [:myco_bot, :ht_sensor, :read_rh],
 
-      #[:myco_bot, :gpio, :error],
-      #[:myco_bot, :gpio, :opened],
-      #[:myco_bot, :gpio, :up],
-      #[:myco_bot, :gpio, :down],
-      #[:myco_bot, :gpio, :sync],
+      # [:myco_bot, :gpio, :error],
+      # [:myco_bot, :gpio, :opened],
+      # [:myco_bot, :gpio, :up],
+      # [:myco_bot, :gpio, :down],
+      # [:myco_bot, :gpio, :sync],
 
-      [:myco_bot_ui, :device, :refresh]
-      #[:myco_bot_ui, :dashboard, :mounted]
+      [:myco_bot_ui, :device, :refresh],
+      [:myco_bot_ui, :device, :change]
+      # [:myco_bot_ui, :dashboard, :mounted]
     ]
 
     :telemetry.attach_many(
@@ -28,7 +29,9 @@ defmodule MycoBot.Instrumenter do
   end
 
   def handle_event([:myco_bot, :ht_sensor, :read_rh], measurements, _meta, _config) do
-    if measurements.rh >= 91, do: MycoBot.GPIO.down(16), else: MycoBot.GPIO.up(16)
+    if measurements.rh >= 95,
+      do: MycoBot.GPIO.down(16),
+      else: MycoBot.GPIO.up(16)
   end
 
   def handle_event([:myco_bot, :ht_sensor, :error], _measurements, _meta, _config) do
@@ -37,6 +40,14 @@ defmodule MycoBot.Instrumenter do
 
   def handle_event([:myco_bot_ui, :device, :refresh], _measurements, _meta, _config) do
     MycoBot.Relay.report_states()
+  end
+
+  def handle_event([:myco_bot_ui, :device, :change], _measurements, meta, _config) do
+    Logger.debug("[MYCOBOT] [:myco_bot_ui, :device, :change]: #{inspect(meta)}")
+
+    if meta.value == 1,
+      do: MycoBot.GPIO.up(meta.pin_number),
+      else: MycoBot.GPIO.down(meta.pin_number)
   end
 
   def handle_event(event, measurements, meta, _config) do
